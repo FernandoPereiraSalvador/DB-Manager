be0eafdbdab098fc33e0628d42aed5df6bdd7529
FernandoPereiraSalvador <fernandopereirasalvador@gmail.com>
2023-10-28 18:05:05 +0200

    Corrección líneas
    Se ha corregido el problema que causaba un bucle infinito al no poder introducir los datos de la conexión

--- app/src/main/java/DBManager/ServerManager.java
+++ app/src/main/java/DBManager/ServerManager.java
@@ -1,7 +1,7 @@
 package DBManager;
 
 // Imports per a entrada de dades
-import java.util.Scanner;
+import static DBManager.Utilidades.leerTextoC;
 
 /**
  * La clase 'ServerManager' es la clase principal que lanza la aplicación. Contiene el método 'main' que recopila información del servidor e inicia una instancia de 'ConnectionManager' para gestionar las conexiones.
@@ -10,62 +10,54 @@ public class ServerManager {
 
     /**
      * Es la funcion principal de la clase 'ServerManager'
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
 
-        ConnectionManager cm;
-
-        Scanner keyboard = new Scanner(System.in);
-
         String user, pass, ip, port, bd;
 
+        ConnectionManager cm = null;
+
         do {
-            
+
             // Solicita la base de datos a utilizar.
-            System.out.print(ConsoleColors.GREEN_BOLD_BRIGHT + "# Base de datos (mysql / mariadb): " + ConsoleColors.BLACK);
-            bd = keyboard.nextLine();
-            
+            bd = leerTextoC(ConsoleColors.GREEN_BOLD_BRIGHT + "# Base de datos (mysql / mariadb): " + ConsoleColors.BLACK);
+
             // Solicita la dirección IP del servidor.
-            System.out.print(ConsoleColors.GREEN_BOLD_BRIGHT + "# Server: " + ConsoleColors.RESET);
-            ip = keyboard.nextLine();
-            
+            ip = leerTextoC(ConsoleColors.GREEN_BOLD_BRIGHT + "# Server: " + ConsoleColors.RESET);
+
             // Solicita el puerto del servidor.
-            System.out.print(ConsoleColors.GREEN_BOLD_BRIGHT + "# Port: " + ConsoleColors.RESET);
-            port = keyboard.nextLine();
-            
+            port = leerTextoC(ConsoleColors.GREEN_BOLD_BRIGHT + "# Port: " + ConsoleColors.RESET);
+
             // Solicita el nombre de usuario para la conexión.
-            System.out.print(ConsoleColors.GREEN_BOLD_BRIGHT + "# Username: " + ConsoleColors.RESET);
-            user = keyboard.nextLine();
-            
+            user = leerTextoC(ConsoleColors.GREEN_BOLD_BRIGHT + "# Username: " + ConsoleColors.RESET);
+
             // Solicita la contraseña del usuario para la conexión.
-            System.out.print(ConsoleColors.GREEN_BOLD_BRIGHT + "# Password: " + ConsoleColors.BLACK);
-            pass = keyboard.nextLine();
+            pass = leerTextoC(ConsoleColors.GREEN_BOLD_BRIGHT + "# Password: " + ConsoleColors.BLACK);
             System.out.print(ConsoleColors.RESET);
-            
-            cm = null;
-            
-            if (ip != null && port != null && user != null && pass != null) {
+
+            if (bd != null && !bd.isEmpty() && ip != null && !ip.isEmpty() && port != null && !port.isEmpty() && user != null && !user.isEmpty() && pass != null) {
                 // Inicializa una instancia de ConnectionManager con los datos proporcionados.
-                try{
-                    cm = new ConnectionManager(ip, port, user, pass,bd);
-                }catch(Exception e){
+                try {
+                    cm = new ConnectionManager(ip, port, user, pass, bd);
+                } catch (Exception e) {
                     System.out.println("Error: " + e.getMessage());
                 }
-                
-            } else if (ip == null && port == null && user == null && pass == null){
+
+            } else if ("".equals(ip) && "".equals(port) && "".equals(user) && "".equals(pass) && "".equals(bd)) {
                 // Inicializa una instancia de ConnectionManager sin datos.
-                try{
+                try {
                     cm = new ConnectionManager();
-                }catch(Exception e){
+                } catch (Exception e) {
                     System.out.println("Error: " + e.getMessage());
                 }
-            }else{
+            } else {
                 System.out.println("Por favor, ingrese todos los datos requeridos.");
             }
 
-        } while (cm.connectDBMS() == null);
-        
+        } while (cm == null || cm.connectDBMS() == null);
+
         // Inicia el shell interactivo.
         cm.startShell();
 
